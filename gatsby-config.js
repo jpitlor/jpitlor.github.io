d842710c9e7e38eeed2d99055dc986a6ee3f8784
Jordan Pitlor <jordan@jordanpitlor.com>
2019-12-23 22:40:07 -0500

    reformatted things

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,44 +1,44 @@
-require('dotenv').config();
+require(`dotenv`).config();
 module.exports = {
 	siteMetadata: {
-		title: 'Jordan Pitlor',
-		description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-		author: 'Jordan Pitlor',
+		title: `Jordan Pitlor`,
+		description: `Software Engineer. Contact me at jordan@pitlor.dev!`,
+		author: `Jordan Pitlor`,
 	},
 	plugins: [
-		'gatsby-plugin-react-helmet',
+		`gatsby-plugin-react-helmet`,
 		{
-			resolve: 'gatsby-source-filesystem',
+			resolve: `gatsby-source-filesystem`,
 			options: {
-				name: 'images',
+				name: `images`,
 				path: `${__dirname}/src/images`,
 			},
 		},
-		'gatsby-transformer-sharp',
-		'gatsby-plugin-sharp',
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
 		{
-			resolve: 'gatsby-plugin-manifest',
+			resolve: `gatsby-plugin-manifest`,
 			options: {
-				name: 'gatsby-starter-default',
-				short_name: 'starter',
-				start_url: '/',
-				background_color: '#663399',
-				theme_color: '#663399',
-				display: 'minimal-ui',
-				icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+				name: `gatsby-starter-default`,
+				short_name: `starter`,
+				start_url: `/`,
+				background_color: `#663399`,
+				theme_color: `#663399`,
+				display: `minimal-ui`,
+				icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
 			},
 		},
 		{
-			resolve: 'gatsby-source-github-api',
+			resolve: `gatsby-source-github-api`,
 			options: {
-				token: 'e246c54ff3233d4e79b7f6050e7e4a4b3089766e',
+				token: process.env.GITHUB_API_TOKEN,
 				variables: {}
 			}
 		},
 		{
-			resolve: 'gatsby-source-contentful',
+			resolve: `gatsby-source-contentful`,
 			options: {
-			  spaceId: '8lixs3gr7ad9',
+			  spaceId: `8lixs3gr7ad9`,
 			  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 			  downloadLocal: true,
 			},
