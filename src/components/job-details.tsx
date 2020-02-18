935269752fade51d71a813ebd2ab0a890367dbaf
Jordan Pitlor <jordan@jordanpitlor.com>
2020-02-17 23:08:42 -0500

    added some job details stuff

--- src/components/job-details.tsx
+++ src/components/job-details.tsx
@@ -1,6 +1,31 @@
 import * as React from "react";
 
-const JobDetails = ({job}) => (
+interface Location {
+    lat: number;
+    lon: number;
+}
+
+interface Job {
+    company: String
+    contentful_id: String
+    description: object // It's...complicated
+    endDate: Date
+    endPay: number
+    isHourly: Boolean
+    location: Location
+    node_locale: String
+    parent: Node
+    spaceId: String
+    startDate: Date
+    startPay: number
+    title: String
+}
+
+interface JobDetailsProps {
+    job: Job | null;
+}
+
+const JobDetails = ({job}: JobDetailsProps) => (
     <div className="panel is-grey column">
         <div className="is-vcentered is-center">
             <span className="has-text-grey is-size-4">Select a job to see more details</span>
--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -1,56 +1,76 @@
 import * as React from 'react';
+import {useState} from "react";
+import groupBy from "lodash.groupby";
 
 import Layout from '../components/layout';
 import { graphql } from 'gatsby';
 import Job from '../components/job';
 import JobDetails from "../components/job-details";
 
-const Experience = ({data: {allContentfulJob: {nodes: jobs}}}) => (
-	<Layout title="Experience">
-		<div className="container">
-			<div className="columns">
-				<div className="timeline column">
-					<header className="timeline-header">
-						<span className="tag is-medium is-primary">Start</span>
-					</header>
-					<div className="timeline-item">
-						<div className="timeline-marker" />
-						<div className="timeline-content">
-							<p className="heading">January 2016</p>
-							<p>Timeline content - Can include any HTML element</p>
+interface AllContentfulJob {
+	nodes: object;
+}
+
+interface Data {
+	allContentfulJob: AllContentfulJob;
+}
+
+interface ExperienceProps {
+	data: Data;
+}
+
+const Experience = ({data: {allContentfulJob: {nodes: jobs}}}: ExperienceProps) => {
+	const [moreDetailsJob, setMoreDetailsJob] = useState(null);
+	const onClickJob = job => () => setMoreDetailsJob(job);
+	const groupedJobs = groupBy(jobs, job => job.startDate);
+
+	return (
+		<Layout title="Experience">
+			<div className="container">
+				<div className="columns">
+					<div className="timeline column">
+						<header className="timeline-header">
+							<span className="tag is-medium is-primary">Start</span>
+						</header>
+						<div className="timeline-item">
+							<div className="timeline-marker" />
+							<div className="timeline-content">
+								<p className="heading">January 2016</p>
+								<p>Timeline content - Can include any HTML element</p>
+							</div>
+						</div>
+						<div className="timeline-item">
+							<div className="timeline-marker is-image is-32x32">
+								<img src="http://bulma.io/images/placeholders/32x32.png" />
+							</div>
+							<div className="timeline-content">
+								<p className="heading">February 2016</p>
+								<p>Timeline content - Can include any HTML element</p>
+							</div>
+						</div>
+						<header className="timeline-header">
+							<span className="tag is-primary">2017</span>
+						</header>
+						<div className="timeline-item">
+							<div className="timeline-marker is-icon">
+								<i className="fa fa-flag" />
+							</div>
+							<div className="timeline-content">
+								<p className="heading">March 2017</p>
+								<p>Timeline content - Can include any HTML element</p>
+							</div>
+						</div>
+						<div className="timeline-header">
+							<span className="tag is-medium is-primary">End</span>
 						</div>
 					</div>
-					<div className="timeline-item">
-						<div className="timeline-marker is-image is-32x32">
-							<img src="http://bulma.io/images/placeholders/32x32.png" />
-						</div>
-						<div className="timeline-content">
-							<p className="heading">February 2016</p>
-							<p>Timeline content - Can include any HTML element</p>
-						</div>
-					</div>
-					<header className="timeline-header">
-						<span className="tag is-primary">2017</span>
-					</header>
-					<div className="timeline-item">
-						<div className="timeline-marker is-icon">
-							<i className="fa fa-flag" />
-						</div>
-						<div className="timeline-content">
-							<p className="heading">March 2017</p>
-							<p>Timeline content - Can include any HTML element</p>
-						</div>
-					</div>
-					<div className="timeline-header">
-						<span className="tag is-medium is-primary">End</span>
-					</div>
+					<JobDetails job={moreDetailsJob} />
 				</div>
-				<JobDetails />
 			</div>
-		</div>
-		{/*{jobs.map(job => <Job data={job} />)}*/}
-	</Layout>
-);
+			{/*{jobs.map(job => <Job data={job} />)}*/}
+		</Layout>
+	);
+};
 
 export default Experience;
 
