2aae0f090dfd4bed76780cebb80206bf6f94f687
Jordan Pitlor <jpitlor@gmail.com>
2020-02-21 09:32:57 -0500

    removed jest since eslint is run on gatsby build anyway

--- jest-preprocess.js
+++
@@ -1,7 +0,0 @@
-const babelJest = require('babel-jest');
-
-const babelOptions = {
-    presets: ['babel-preset-gatsby', '@babel/preset-typescript'],
-};
-
-module.exports = babelJest.createTransformer(babelOptions);
