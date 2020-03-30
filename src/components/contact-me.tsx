7b55662ca42c3651f865879bad03d3e526fff94b
Jordan Pitlor <jordan@jordanpitlor.com>
2020-03-30 15:21:02 -0400

    job details fixes

--- src/components/contact-me.tsx
+++ src/components/contact-me.tsx
@@ -1,10 +1,6 @@
 import * as React from "react";
 import Section from "./section";
 
-const iconStyles = {
-
-};
-
 const ContactMe = () => (
     <Section title="Contact Me">
         <p className="is-tight-desktop">
@@ -14,32 +10,32 @@ const ContactMe = () => (
         <br />
         <br />
         <div className="columns is-mobile">
-            <div className="icon is-large column" style={iconStyles}>
+            <div className="icon is-large column has-text-centered">
                 <a href="mailto:jordan@pitlor.dev">
                     <i className="far fa-envelope is-size-1 is-size-3-mobile" />
                 </a>
             </div>
-            <div className="icon is-large column" style={iconStyles}>
+            <div className="icon is-large column has-text-centered">
                 <a href="tel:+12164038126">
                     <i className="far fa-phone is-size-1 is-size-3-mobile" />
                 </a>
             </div>
-            <div className="icon is-large column" style={iconStyles}>
+            <div className="icon is-large column has-text-centered">
                 <a href="https://www.linkedin.com/in/jordan-pitlor/">
                     <i className="fab fa-linkedin is-size-1 is-size-3-mobile" />
                 </a>
             </div>
-            <div className="icon is-large column" style={iconStyles}>
+            <div className="icon is-large column has-text-centered">
                 <a href="https://www.facebook.com/jordan.pitlor">
                     <i className="fab fa-facebook is-size-1 is-size-3-mobile" />
                 </a>
             </div>
-            <div className="icon is-large column" style={iconStyles}>
+            <div className="icon is-large column has-text-centered">
                 <a href="https://twitter.com/j__pitty">
                     <i className="fab fa-twitter is-size-1 is-size-3-mobile" />
                 </a>
             </div>
-            <div className="icon is-large column" style={iconStyles}>
+            <div className="icon is-large column has-text-centered">
                 <a href="https://www.instagram.com/j__pitty/">
                     <i className="fab fa-instagram is-size-1 is-size-3-mobile" />
                 </a>
--- src/components/job-details.tsx
+++ src/components/job-details.tsx
@@ -19,17 +19,25 @@ const JobDetails = ({job, locationId}: JobDetailsProps) => (
             <React.Fragment>
                 <p className="title is-2">{job.company}</p>
                 <p className="subtitle is-4">{job.title}</p>
-                <div className="content">
-                    {documentToReactComponents(job.description?.json)}
-                </div>
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        minHeight: "calc(100% - 6.5rem)",
+                    }}
+                >
+                    <div className="content" style={{flex: 1}}>
+                        {documentToReactComponents(job.description?.json)}
+                    </div>
 
-                <iframe
-                    height="450"
-                    frameBorder="0"
-                    style={{border: 0, width: "100%"}}
-                    src={`https://www.google.com/maps/embed/v1/place?q=place_id:${locationId}&key=${process.env.GATSBY_GOOGLE_MAPS_API_TOKEN}`}
-                    allowFullScreen={true}
-                />
+                    <iframe
+                        height="450"
+                        frameBorder="0"
+                        style={{border: 0, width: "100%"}}
+                        src={`https://www.google.com/maps/embed/v1/place?q=place_id:${locationId}&key=${process.env.GATSBY_GOOGLE_MAPS_API_TOKEN}`}
+                        allowFullScreen={true}
+                    />
+                </div>
             </React.Fragment>
         ) : (
             <div
