6ad563cec3acd4f6a2457dac32d7166ffbeace58
Jordan Pitlor <jordan@jordanpitlor.com>
2020-03-30 15:46:14 -0400

    remove missed styled component

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,20 +1,11 @@
 import React, {useState, useEffect} from "react";
 import {PDFViewer} from "@react-pdf/renderer";
-import styled from "styled-components";
 
 import Resume from "../templates/resume";
 import {useJobs} from "../utils/useJobs";
 import useSchools from "../utils/useSchools";
 import useProjects from "../utils/useProjects";
 import SEO from "../components/seo";
-
-const Container = styled.div`
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100vw;
-    height: 100vh;
-`;
 
 const ResumePage = () => {
     const [inBrowser, setInBrowser] = useState(false);
@@ -27,12 +18,20 @@ const ResumePage = () => {
     }, [jobs]);
 
     return (
-        <Container>
+        <div
+            style={{
+                position: "absolute",
+                left: 0,
+                top: 0,
+                width: "100vw",
+                height: "100vh",
+            }}
+        >
             <SEO title="Resume" />
             {inBrowser && <PDFViewer height="100%" width="100%">
                 <Resume jobs={jobs} projects={projects} schools={schools} />
             </PDFViewer>}
-        </Container>
+        </div>
     );
 }
 
