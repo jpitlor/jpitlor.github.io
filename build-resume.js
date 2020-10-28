1355a7b50742e3bb0dacf836e33fb05af0119db4
Jordan Pitlor <jpitlor@gmail.com>
2020-10-27 22:27:53 -0400

    fix resume build

--- build-resume.js
+++ build-resume.js
@@ -1,14 +1,25 @@
+import ReactPDF from "@react-pdf/renderer";
+import fs from "fs";
 import React from "react";
-import ReactPDF from '@react-pdf/renderer';
-import fs from 'fs';
 
 import Resume from "./src/resume";
 
 const {
   jobs,
   projects,
-  schools,
+  schools
 } = JSON.parse(fs.readFileSync("./resume-data.json", "utf-8"));
+
+for (let i = 0; i < jobs.length; i++) {
+  if (jobs[i].startDate) {
+    jobs[i].startDate = new Date(jobs[i].startDate);
+  }
+  
+  if (jobs[i].endDate) {
+    jobs[i].endDate = new Date(jobs[i].endDate);
+  }
+}
+
 ReactPDF.render(
   <Resume jobs={jobs} projects={projects} schools={schools} />,
   `${__dirname}/docs/Jordan Pitlor Resume.pdf`
